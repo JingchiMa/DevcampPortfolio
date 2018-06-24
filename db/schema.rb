f6d7830f057d5fc27139411a6e055556ab704e79
Jingchi Ma <34053929+JingchiMa@users.noreply.github.com>
2018-06-24 09:53:28 -0700

    Created Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_24_040328) do
+ActiveRecord::Schema.define(version: 2018_06_24_050235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2018_06_24_040328) do
     t.string "subtitle"
     t.text "body"
     t.text "main_image"
-    t.text "thum_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
